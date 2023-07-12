5039b4dd4895494a4ae5ee6cc4c3c041d708019e
Max Horstmann <you@example.com>
2023-07-12 15:41:17 +0200

    added workshop

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import { getRecipe } from '../components/contentful/recipes';
 import { DefaultLayout } from '../components/layouts/DefaultLayout';
+import Workshop from '../components/workshop/Workshop.jsx';
 
 export const Home = () => {
   getRecipe();
   return (
     <DefaultLayout>
       <h1>Home</h1>
+
+      <Workshop />
     </DefaultLayout>
   );
 };
