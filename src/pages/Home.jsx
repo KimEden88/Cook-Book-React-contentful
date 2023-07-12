e497b3cef3109b557cbd8bd78022cc53018636de
Armi Mujica <armivolk@gmail.com>
2023-07-12 17:17:35 +0200

    Newsletter Input First Commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
-import { getRecipe } from '../components/contentful/recipes';
-import { DefaultLayout } from '../components/layouts/DefaultLayout';
+import { SubscribeInput } from "../Subscribe/SubscribeInput";
+import { getRecipe } from "../components/contentful/recipes";
+import { DefaultLayout } from "../components/layouts/DefaultLayout";
 
 export const Home = () => {
   getRecipe();
   return (
     <DefaultLayout>
       <h1>Home</h1>
+      <SubscribeInput />
     </DefaultLayout>
   );
 };
