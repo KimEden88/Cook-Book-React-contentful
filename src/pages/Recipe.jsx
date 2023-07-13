507ad44752e53859ae6b1ce716c3f8fd3da867fc
Armi Mujica <armivolk@gmail.com>
2023-07-13 18:27:25 +0200

    Material UI Recipe

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -1,9 +1,13 @@
-import { DefaultLayout } from '../components/layouts/DefaultLayout';
+import { DefaultLayout } from "../components/layouts/DefaultLayout";
+import Sidebar from "../components/sidebar/Sidebar";
 
 export const Recipe = () => {
   return (
     <DefaultLayout>
-      <h1>Recipe</h1>
+      <div>
+        <h1>Recipe</h1>
+        <Sidebar />
+      </div>
     </DefaultLayout>
   );
 };
