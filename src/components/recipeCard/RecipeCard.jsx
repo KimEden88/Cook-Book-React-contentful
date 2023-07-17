063cca87e2667d8f88727667bcb05c3e0a8a5c23
kim <laylaone22@gmail.com>
2023-07-17 13:43:10 +0200

    small fix

--- src/components/recipeCard/RecipeCard.jsx
+++ src/components/recipeCard/RecipeCard.jsx
@@ -19,62 +19,70 @@ export const RecipeCard = () => {
   }, []);
 
   return (
-    <Grid
-      className="grid"
-      container
-      rowSpacing={1}
-      columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-      justifyContent={'center'}
-    >
-      {recipes &&
-        recipes.items.map((item) => (
-          <Card
-            className={item.sys.id}
-            sx={{
-              width: 345,
-              margin: 2,
-            }}
-          >
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="190"
-                image={`https:` + item.fields.image.fields.file.url}
-                alt="Recipe"
-              />
-              <CardContent className="cardContent">
-                <Typography
-                  className="title"
-                  gutterBottom
-                  variant="h5"
-                  component="div"
+    <>
+      <h1
+        id="recipe"
+        className="recipe"
+      >
+        Our Recipes
+      </h1>
+      <Grid
+        className="grid"
+        container
+        rowSpacing={1}
+        columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+        justifyContent={'center'}
+      >
+        {recipes &&
+          recipes.items.map((item) => (
+            <Card
+              className={item.sys.id}
+              sx={{
+                width: 345,
+                margin: 2,
+              }}
+            >
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="190"
+                  image={`https:` + item.fields.image.fields.file.url}
+                  alt="Recipe"
+                />
+                <CardContent className="cardContent">
+                  <Typography
+                    className="title"
+                    gutterBottom
+                    variant="h5"
+                    component="div"
+                  >
+                    {item.fields.title}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="text.secondary"
+                  >
+                    {item.fields.description}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button
+                  className="btn"
+                  size="small"
+                  color="primary"
                 >
-                  {item.fields.title}
-                </Typography>
-                <Typography
-                  variant="body2"
-                  color="text.secondary"
-                >
-                  {item.fields.description}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button
-                className="btn"
-                size="small"
-                color="primary"
-              >
-                <Link
-                  className="link"
-                  to={`/recipe/${item.sys.id}`}
-                >
-                  Go to Recipe
-                </Link>
-              </Button>
-            </CardActions>
-          </Card>
-        ))}
-    </Grid>
+                  <Link
+                    className="link"
+                    to={`/recipe/${item.sys.id}`}
+                  >
+                    Go to Recipe
+                  </Link>
+                </Button>
+              </CardActions>
+            </Card>
+          ))}
+      </Grid>
+    </>
   );
 };
