2b188e224e366cd361ef04ad18afbf113a4c2287
Max Horstmann <you@example.com>
2023-07-17 22:09:38 +0200

    finished About-section

--- src/components/infosection/About.jsx
+++ src/components/infosection/About.jsx
@@ -5,46 +5,55 @@ import Yuna from './petImg/Yuna.jpg';
 
 export default function About() {
   return (
-    <div className="frame">
-      <div className="aboutCard">
-        <img
-          src={Julie}
-          alt="picJulie"
-        />
-        <h3>Julie</h3>
-        <p>Just hoped for some free food</p>
+    <>
+      <div className="frame">
+        <h1 className="petTitle">About Us</h1>
+        <div className="aboutCard">
+          <h3 className="petName">Julie</h3>
+          <img
+            className="petPics"
+            src={Julie}
+            alt="picJulie"
+          />
 
-        <div>
-          Favourite food:
-          <p>Boiled egg!</p>
+          <p className="petDescr">
+            I'm here because I was hoping there'd be free food
+          </p>
+
+          <p className="faveFood">Favourite food:</p>
+          <p className="foodAnsw">Boiled egg!</p>
+        </div>
+        <div className="aboutCard">
+          <h3 className="petName">Leia</h3>
+          <img
+            className="petPics"
+            src={Leia}
+            alt="pic"
+          />
+
+          <p className="petDescr">
+            Curious to know what delicious treats could be prepared
+          </p>
+
+          <p className="faveFood">Favourite food:</p>
+          <p className="foodAnsw">Steak!</p>
+        </div>
+        <div className="aboutCard">
+          <h3 className="petName">Yuna</h3>
+          <img
+            className="petPics"
+            src={Yuna}
+            alt="pic"
+          />
+
+          <p className="petDescr">
+            On the journey of finding the best tuna recipes
+          </p>
+
+          <p className="faveFood">Favourite food:</p>
+          <p className="foodAnsw">Tuna with mussels!</p>
         </div>
       </div>
-      <div className="aboutCard">
-        <img
-          src={Leia}
-          alt="pic"
-        />
-        <h3>Leia</h3>
-        <p>Curious to know what delicious treats could be prepared</p>
-
-        <div>
-          Favourite food:
-          <p>Steak!</p>
-        </div>
-      </div>
-      <div className="aboutCard">
-        <img
-          src={Yuna}
-          alt="pic"
-        />
-        <h3>Yuna</h3>
-        <p>On the journey of finding the best tuna recipes</p>
-
-        <div>
-          Favourite food:
-          <p>Tuna with mussels!</p>
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -1,10 +1,9 @@
 import { DefaultLayout } from '../components/layouts/DefaultLayout';
-import { About } from '../components/infosection/About';
+import About from '../components/infosection/About';
 
 export const Info = () => {
   return (
     <DefaultLayout>
-      <h1>Info</h1>
       <About />
     </DefaultLayout>
   );
